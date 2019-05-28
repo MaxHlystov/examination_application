39fe2afbd10348e695e94ba9721cf78e9ab4828f
Maxim <maxvls@gmail.com>
2019-05-28 23:35:56 +0300

    Update Main.java

--- src/main/java/ru/fmtk/hlystov/hw_examination_app/Main.java
+++ src/main/java/ru/fmtk/hlystov/hw_examination_app/Main.java
@@ -4,9 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
-        IQuestion q = context.getBean(Question.class);
-        System.out.println(q.getCount());
+
     }
 
 }
