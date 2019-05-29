13228b7ab727fd9d6921a6b04be02baed40e2df0
MaxHlystov <maxvls@gmail.com>
2019-05-29 23:31:18 +0300

    hw1 created

--- src/main/java/ru/fmtk/hlystov/hw_examination_app/Main.java
+++ src/main/java/ru/fmtk/hlystov/hw_examination_app/Main.java
@@ -4,7 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Examination application!");
     }
 
 }
